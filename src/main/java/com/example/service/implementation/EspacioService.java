152a8817d4d22082fe9adf56e36022edd297c939
CristianGrageda <72391157+CristianGrageda@users.noreply.github.com>
2022-06-12 01:55:46 -0300

    master: Se agrega autowired en EspacioService

--- src/main/java/com/example/service/implementation/EspacioService.java
+++ src/main/java/com/example/service/implementation/EspacioService.java
@@ -2,6 +2,7 @@ package com.example.service.implementation;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.entities.Espacio;
 import com.example.repo.IEspacioRepository;
@@ -9,6 +10,7 @@ import com.example.repo.IEspacioRepository;
 @Service
 public class EspacioService {
 	
+	@Autowired
 	private IEspacioRepository repo;
 	
 	public List<Espacio> traer() {
