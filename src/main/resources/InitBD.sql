fa4f0064316b6c20f77c1769399b2b1382615da5
Nahuel fernandez <90975885+nafernandez12@users.noreply.github.com>
2022-06-22 01:15:20 -0300

    Ultimos cambios

--- src/main/resources/InitBD.sql
+++ src/main/resources/InitBD.sql
@@ -3,8 +3,7 @@
     1.- Se debe eliminar la BD en caso de que exista, asi se crea desde cero
     2.- Se debe crear la BD (Solo crear la BD, no insertar nada por ahora. La tablas se crean desde java)
     3.- Antes de inicar el Proyecto, verificar en el archivo application.properties en user y pass de la BD
-    4.- Iniciar el Proyecto TEST Java (DemoApplicationTest.java), asi se crean las tablas y el primer
-		usuario Admin (user: polo - pass: 9999)
+    4.- Iniciar el Proyecto TEST Java (DemoApplicationTest.java), asi se crean las tablas y los usuarios
 	5.- Una vez terminada la ejecucion de DemoApplicationTest.java, Ejecutar los INSERT's para tener datos 
 		precargados en el proyecto
 	6.- Por ultimo ejecutar DemoApplication.java lo cual dará inicio al proyecto
