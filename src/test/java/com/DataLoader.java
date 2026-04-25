a94f5165952158acdf4ee0ef7a06880d80db2a1c
agustin0512 <agustin0512@gmail.com>
2026-04-24 22:43:40 -0300

    fix subir railway

--- src/test/java/com/DataLoader.java
+++ src/test/java/com/DataLoader.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +11,7 @@ import com.example.entities.User;
 import com.example.entities.UserRole;
 import com.example.service.implementation.UserRoleService;
 import com.example.service.implementation.UserService;
-@Profile("dev")
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
@@ -24,37 +23,23 @@ public class DataLoader implements CommandLineRunner {
 
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    @Override
-    public void run(String... args) throws Exception {
+   @Override
+public void run(String... args) throws Exception {
 
-        if (userService.count() > 0) return;
+    if (userService.count() > 0) return;
 
+    UserRole rolAdmin = new UserRole();
+    rolAdmin.setRole("ROLE_ADMIN");
+    userRoleService.guardar(rolAdmin);
 
-        UserRole rolUser = new UserRole();
-        UserRole rolAdmin = new UserRole();
-        UserRole rolAudit = new UserRole();
+    User user = new User();
+    user.setUsername("polo");
+    user.setPassword(encoder.encode("9999"));
+    user.setEnabled(true);
+    user.setCreatedAt(LocalDateTime.now());
+    user.setUpdatedAt(LocalDateTime.now());
+    user.setRol(rolAdmin);
 
-        rolUser.setRole("ROLE_USER");
-        rolAdmin.setRole("ROLE_ADMIN");
-        rolAudit.setRole("ROLE_AUDIT");
-
-        userRoleService.guardar(rolUser);
-        userRoleService.guardar(rolAdmin);
-        userRoleService.guardar(rolAudit);
-
-        User user = new User();
-        user.setNombre("Juan");
-        user.setApellido("Perez");
-        user.setMail("prueba@gmail.com");
-        user.setTipodoc("DNI");
-        user.setDni(403414);
-        user.setUsername("polo");
-        user.setPassword(encoder.encode("9999"));
-        user.setEnabled(true);
-        user.setCreatedAt(LocalDateTime.now());
-        user.setUpdatedAt(LocalDateTime.now());
-        user.setRol(rolAdmin);
-
-        // ... resto igual
-    }
+    userService.guardar(user); // 👈 CLAVE
+}
 }
