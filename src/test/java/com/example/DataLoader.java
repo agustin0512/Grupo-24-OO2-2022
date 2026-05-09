a4c2b79bb683afb07de9088a974092f01b64a74f
agustin0512 <agustin0512@gmail.com>
2026-05-08 21:29:50 -0300

    dataloader log

--- src/test/java/com/example/DataLoader.java
+++ src/test/java/com/example/DataLoader.java
@@ -14,7 +14,7 @@ import com.example.service.implementation.UserService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
-
+    System.out.println("🔥 DATA LOADER EJECUTANDOSE 🔥");
     @Autowired
     private UserService userService;
 
